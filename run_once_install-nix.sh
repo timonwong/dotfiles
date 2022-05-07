1f98621206eb84756a9c419b35a5864d3c187c16
Timon Wong <timon86.wang@gmail.com>
2022-05-07 13:53:46 +0800

    do not install nix
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- run_once_install-nix.sh
+++ run_once_install-nix.sh
@@ -2,13 +2,13 @@
 
 set -eu -o pipefail
 
-if [[ ! -d /nix/store ]]; then
-    {{ if eq .chezmoi.os "darwin" -}}
-    sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
-    {{ else -}}
-    sh <(curl -L https://nixos.org/nix/install)
-    {{ end }}
-fi
+#if [[ ! -d /nix/store ]]; then
+#    {{ if eq .chezmoi.os "darwin" -}}
+#    sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume
+#    {{ else -}}
+#    sh <(curl -L https://nixos.org/nix/install)
+#    {{ end }}
+#fi
 
 # if [[ ! -e ~/.nix-profile/bin/home-manager ]]; then
 #     . ~/.nix-profile/etc/profile.d/nix.sh;

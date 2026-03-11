fd1aaef2441426e2f82e13bd0a0f41328689a918
timonwong <timon86.wang@gmail.com>
2026-03-11 21:29:58 +0800

    fix(op): avoid plugin alias setup when locale is unavailable

--- private_dot_config/private_op/plugins.sh
+++ private_dot_config/private_op/plugins.sh
@@ -1,4 +1,6 @@
 # shellcheck disable=all
 
-export OP_PLUGIN_ALIASES_SOURCED=1
-alias glab="op plugin run -- glab"
+if command -v locale >/dev/null 2>&1; then
+    export OP_PLUGIN_ALIASES_SOURCED=1
+    alias glab="op plugin run -- glab"
+fi

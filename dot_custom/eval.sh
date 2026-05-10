83ec6552d71240c5ed3a8e7f12aab846ae1a6617
timonwong <timon86.wang@gmail.com>
2026-05-10 23:17:02 +0800

    remove navi
    Signed-off-by: timonwong <timon86.wang@gmail.com>

--- dot_custom/eval.sh
+++ dot_custom/eval.sh
@@ -30,7 +30,6 @@ eval_if_cmd_exists "starship" starship init zsh
 eval_if_cmd_exists "fzf" fzf --zsh
 # Load sheldon after fzf so fzf-tab keeps Tab completion ownership.
 eval_if_cmd_exists "sheldon" sheldon source
-eval_if_cmd_exists "navi" navi widget zsh
 eval_if_cmd_exists "zoxide" zoxide init zsh
 eval_if_mise_activate
 eval_if_cmd_exists "atuin" atuin init zsh
